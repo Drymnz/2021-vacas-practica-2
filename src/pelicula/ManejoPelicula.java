39277ba77ede3b5d71152ffa2cc5ad788f3343c8
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-06-16 20:43:15 -0600

    fue creado los archivos pelicula y manejo pelicula

---
+++ src/pelicula/ManejoPelicula.java
@@ -0,0 +1,7 @@
+package src.pelicula;
+public class ManejoPelicula {
+    private Pelicula[] catalogo = new Pelicula[50];
+    private int siguienteCodigo;
+
+    
+}
---
+++ src/pelicula/Pelicula.java
@@ -0,0 +1,4 @@
+package src.pelicula;
+public class Pelicula {
+    
+}
